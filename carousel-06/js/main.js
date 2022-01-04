3f507ac17f0faa6bc1489d8bad7f7eac12275d27
VigneshSrinivas <90258283+VigneshSrinivas@users.noreply.github.com>
2022-01-04 13:39:56 +0530

    carosel

---
+++ carousel-06/js/main.js
@@ -0,0 +1,43 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	var carousel = function() {
+		$('.featured-carousel').owlCarousel({
+	    loop:true,
+	    autoplay: true,
+	    margin:30,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:true,
+	    dots: false,
+	    autoplayHoverPause: true,
+	    items: 1,
+			navText: ["<p><small style='color:white'>Prev</small><span class='ion-ios-arrow-round-back' style='color:white'></span></p>","<p><small style='color:white'>Next</small><span class='ion-ios-arrow-round-forward' style='color:white'></span></p>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:1
+	      },
+	      1000:{
+	        items:1
+	      }
+	    }
+		});
+
+	};
+	carousel();
+
+})(jQuery);
